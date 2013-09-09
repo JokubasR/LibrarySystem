d53a461074ec1e60a01416d2c9f85c1e739817c1
jo2kas <jokubas.rama@gmail.com>
2013-09-09 13:08:46 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jokubas
+ * @since 2013.09.09
+ * Task: Library: staff, book catalogue, customers. *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
