d07865a44f68012a19ee8c24c118ebf265ebebad
Jokubas Ramanauskas <jokubas.rama@gmail.com>
2013-11-02 17:27:25 +0200

    Create table sql added

---
+++ Db/create.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS users, book, bookLog, genres, bookGenres;
+
+CREATE TYPE ROLES AS ENUM('USER', 'WORKER');
+
+CREATE TABLE IF NOT EXISTS users (
+    id serial PRIMARY KEY, 
+    firstname VARCHAR(80),
+    lastname VARCHAR(80),
+    userRole ROLES
+);
+
+CREATE TABLE IF NOT EXISTS book (
+    id serial PRIMARY KEY, 
+    isbn VARCHAR(40),
+    title VARCHAR(255),
+    author VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS bookLog (
+	id serial PRIMARY KEY,
+	userId INT references users(id),
+	bookId INT references book(id),
+	dateBegin DATE,
+	dateEnd DATE
+);
+
+CREATE TABLE IF NOT EXISTS genres (
+	id serial PRIMARY KEY,
+	name VARCHAR(80)
+);
+
+CREATE TABLE IF NOT EXISTS bookGenres (
+	bookId INT NOT NULL,
+	genreId INT NOT NULL,
+	PRIMARY KEY(bookId, genreId)
+);
